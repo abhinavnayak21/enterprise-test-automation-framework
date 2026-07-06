daa1c51fc53b823a02c5e86be11aeb97f6214e20
abhinavnayak21 <abhinavnayak21@gmail.com>
2026-07-06 20:38:29 +0530

    implement base page

---
+++ ui/locators/login_page.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginPageLocators:
+    """Locators for the OrangeHRM login page."""
+
+    USERNAME_INPUT = (By.NAME, "username")
+
+    PASSWORD_INPUT = (By.NAME, "password")
+
+    LOGIN_BUTTON = (
+        By.CSS_SELECTOR,
+        "button[type='submit']",
+    )
+
+    DASHBOARD_HEADER = (
+        By.XPATH,
+        "//h6[text()='Dashboard']",
+    )
