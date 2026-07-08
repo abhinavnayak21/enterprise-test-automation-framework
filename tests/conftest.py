ceeb947e5e38632ba9b7d05902cb72bf18fb798d
abhinavnayak21 <abhinavnayak21@gmail.com>
2026-07-08 13:29:14 +0530

    captured screenshot on test faliure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 from utils.driver_factory import DriverFactory
+from utils.screenshot import ScreenshotManager
 
 
 @pytest.fixture(scope="function")
@@ -13,3 +14,18 @@ def driver():
     yield driver
 
     driver.quit()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    """
+    Capture a screenshot on test failure.
+    """
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+        driver = item.funcargs.get("driver")
+
+        if driver:
+            ScreenshotManager.save(driver, item.name)
